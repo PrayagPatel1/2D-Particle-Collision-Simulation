47cfb6b0e2bde32cba94cea958f8536a4f82b4e7
Prayag Patel <prayagpatel283@gmail.com>
2026-03-04 13:01:44 -0500

    Able to create multiple particle structs and render them on the screen.

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <raylib.h>
 
 #define WINDOW_WIDTH 900
 #define WINDOW_HEIGHT 500
 #define FPS 60
 #define GRAVITY 120.0f
+#define NUM_OF_PARTICLES 3
 
 typedef struct {
 	Vector2 position;
@@ -13,9 +15,29 @@ typedef struct {
 	float radius;
 }Particle;
 
-void renderParticle(Particle *ptr_particle)
+
+void initializeParticles(Particle *ptr_p)
 {
-	DrawCircleV(ptr_particle->position, ptr_particle->radius, MAROON);
+	for (size_t idx = 0; idx < NUM_OF_PARTICLES; idx++)
+	{
+		Particle particle;
+		particle.position = (Vector2) {WINDOW_WIDTH/2 + idx * 50, WINDOW_HEIGHT/2 + idx * 50};
+		particle.velocity = (Vector2) {0.0f, 0.0f};
+		particle.acceleration = (Vector2) {0.0f, 0.0f};
+		particle.radius = 5.0f;
+
+		ptr_p[idx] = particle;
+	}
+
+}
+
+void renderParticles(Particle *ptr_p)
+{
+	for(size_t idx = 0; idx < NUM_OF_PARTICLES; idx ++)
+	{
+		DrawCircleV(ptr_p[idx].position, ptr_p[idx].radius, BLACK);
+	}
+
 }
 
 void updateParticleDynamic(Particle *ptr_particle, float delta_t)
@@ -41,7 +63,7 @@ void handleWindowCollision(Particle *ptr_particle)
 	//		 the collisions onece every frame and continuously. 
 	
 	// Handles when the particle is going beyond the left and right borders. 
-	if ( (ptr_particle->position.x + ptr_particle->radius) >= WINDOW_WIDTH || 
+	if ((ptr_particle->position.x + ptr_particle->radius) >= WINDOW_WIDTH || 
 			(ptr_particle->position.x - ptr_particle->radius) <= 0) 
 	{
 		ptr_particle->velocity.x *= -1;
@@ -56,16 +78,6 @@ void handleWindowCollision(Particle *ptr_particle)
 
 }
 
-void handleWindowCollisionV2(Particle *ptr_particle)
-{
-	//This version of handling collisions between a particle and a window
-	//border is going to be a continuous collision detection (CCD) compared
-	//to the previous version which what a discrete collision detection (DCD).
-	
-
-
-}
-
 int main(void)
 {
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "2D Collision Particle Simulator");
@@ -73,16 +85,8 @@ int main(void)
 
 	float delta_time = (float) 1/FPS;
 
-	printf("TimeStep: %f\n", delta_time);
-
-	Particle p0 ={
-
-		.position = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2},
-		.velocity = {90.0f, -100.0f}, 
-		.acceleration = {0.0f, GRAVITY},
-		.radius = 6
-
-	};
+	Particle particles[NUM_OF_PARTICLES]; 
+	initializeParticles(particles);
 
 	// Simulation Event Loop
 	while ( !WindowShouldClose() )
@@ -90,9 +94,7 @@ int main(void)
 		BeginDrawing();
 
 			ClearBackground(LIGHTGRAY);
-			renderParticle(&p0);
-			updateParticleDynamic(&p0, delta_time);
-			handleWindowCollision(&p0);
+			renderParticles(particles);
 
 		EndDrawing();
 	}
