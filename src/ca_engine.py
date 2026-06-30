f41a8307e740e89b99a4cad8d95764e5852da7a1
Prayag Patel <prayagpatel283@gmail.com>
2026-06-30 11:56:05 -0400

    Updated the docstring for the CaHashEngine class, implemented a private method called _apply_rule_set(), implemented a public method called update(), written surface level docstrings to both newly created methods and to the render() method as well. Updated the visual representation of an alive cell to be a block character and a dead cell to be a space character.

--- src/ca_engine.py
+++ src/ca_engine.py
@@ -1,13 +1,14 @@
-""" Celluar Automata Engine 
+""" Celluar Automata Hash Engine 
 
 This module provides a celluar automata engine that defines what a cell is and
 a manager to coordinate a collection of cells organized in a grid. Additionally, 
-it contains different rules that defines behaviours of celluar automata. 
+it contains different rules that defines behaviours of celluar automata. As well
+, this engine will be used to hash a password. 
 
 Exported Classes:
-
 Cell: a single unit with a binary state of "alive" or "dead". 
-CAEngine: a global manager of a collection of Cells and defines different rules.
+CaHashEngine: a global manager of a collection of Cells that uses elementary 
+              celluar automata to hash a password.
 """
 
 import random
@@ -16,7 +17,7 @@ CELL_ALIVE = 1
 CELL_DEAD = 0
 
 class Cell:
-    """ 
+    """
     A single bit that lives in a one dimension array with a binary state of 
     "dead" or "alive".
 
@@ -31,8 +32,11 @@ class Cell:
         "alive" and zero represents the cell is "dead".
     """
 
-    def __init__(self) -> None:
-        self._state = random.randint(CELL_DEAD, CELL_ALIVE)
+    def __init__(self, state=None) -> None:
+        if state == None:
+            self._state = random.randint(CELL_DEAD, CELL_ALIVE)
+        else:
+            self._state = state
     
     def get_state(self) -> int:
         """Returns the state of the current Cell object."""
@@ -54,33 +58,70 @@ class CaHashEngine:
     array_length : int
         The length of an array of Cell objects.
     num_of_gen : int
-        Number of total times for the inital array of cells to evolve. 
+        Number of total times for the inital array of cells to evolve.
+
+    Methods
+    -------
+    update
+        Updates the inital array of cells using a specific ruleset. 
+    render
+        Renders a 2D image representing how each cell has evolved through
+        the rulesets. Each row represents a new generation of the previous
+        cells. 
     """
 
     def __init__(self, array_length: int, num_of_gen: int) -> None:
+
+        # TODO: Would it be a good design choice to completely limit the client
+        #       from being able to create more than one instance of CaHashEngine?
+
         self.array_length = array_length
         self.num_of_gen = num_of_gen
 
         cell_array = [Cell() for _ in range(self.array_length)]
         self._cell_matrix = [cell_array if i == 0 else [0] * self.array_length for i in range(self.num_of_gen)]
     
-    def rule_set_30(self, cell1: Cell, cell2: Cell, cell3: Cell) -> int:
-        return cell1.get_state() ^ (cell2.get_state() | cell3.get_state())
+    def _apply_rule_set(self, old_arr: list[Cell]) -> list[Cell]:
+        new_arr = []
+        new_state = 0
+        for cell_idx in range(self.array_length):
+            if cell_idx == 0:
+                new_state = CELL_DEAD ^ (old_arr[cell_idx].get_state() | old_arr[cell_idx + 1].get_state())
+                new_arr.append(Cell(new_state))
+            elif cell_idx == self.array_length - 1:
+                new_state = old_arr[cell_idx - 1].get_state() ^ (old_arr[cell_idx].get_state() | CELL_DEAD)
+                new_arr.append(Cell(new_state))
+            else:
+                new_state = old_arr[cell_idx - 1].get_state() ^ (old_arr[cell_idx].get_state() | old_arr[cell_idx + 1].get_state())
+                new_arr.append(Cell(new_state))
+
+        return new_arr 
     
-    
+    def update(self) -> None:
+        """Updates the previous array of cells using a ruleset."""
+        for gen_idx in range(1, self.num_of_gen):
+            self._cell_matrix.pop(gen_idx)
+            new_arr = self._apply_rule_set(self._cell_matrix[gen_idx - 1])
+            self._cell_matrix.insert(gen_idx, new_arr)
 
     def render(self) -> None:
+        """
+        Renders every array of cells which has evolved to the terminal where a 
+        "dead cell" is represented by a space character and an "alive" cell is 
+        represented by a block character.  
+        """
         for gen_idx in range(self.num_of_gen):
             for cell_idx in range(self.array_length):
                 if self._cell_matrix[gen_idx][cell_idx].get_state() == CELL_ALIVE:
-                    print("O", end="")
+                    print("\u2588", end="")
                 elif self._cell_matrix[gen_idx][cell_idx].get_state() == CELL_DEAD:
-                    print(".", end="")
+                    print(" ", end="")
 
             print("\n")
 
 if __name__ == "__main__":
-    engine = CaHashEngine(10, 1)
+    engine = CaHashEngine(100, 100)
+    engine.update()
     engine.render()
 
     
