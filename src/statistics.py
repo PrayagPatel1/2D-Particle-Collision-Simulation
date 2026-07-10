ecb96b5501b6af9a69e4b0b89b70396e9e515195
Prayag Patel <prayagpatel283@gmail.com>
2026-07-10 11:52:07 -0400

    Implemented all major statistic functions needed (may implement more in the future) that describes the performance of CaHashEngine. Completed file documentation and docstrings for each function.

--- src/statistics.py
+++ src/statistics.py
@@ -4,36 +4,95 @@ This module provides functions to record statistics of the performance of the
 CaHashEngine class. 
 
 Exported Function: 
-count_byte_frequency : 
-hist_of_byte_freq :
-chi_square_statistc : 
-byte_pair_heatmap : 
-bit_balance_statistic : 
+count_byte_frequency : Counts the number of times that the same byte has occured.
+hist_of_byte_freq : Plots the contents of a frequency map as a histogram.
+chi_square_statistc : Calculates the Chi-Square goodness of fit statistic and 
+                      plots the distribution. 
+byte_pair_heatmap : Plots the frequency of a pair of consecutive bytes that have
+                    occured in the hashed passwords as a heatmap.
+bit_balance_statistic : Determines the percentage of howmany times a 1 bit and a
+                        0 bit has occured in the array of hashed passwords.0
+hash_collision_statistic : Determines the number of times that two seperate 
+                           input characters have returned the same hash. 
 """
+
+# Third-party Imports
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chi2
 
+# Custom Implemented Imports
 from txt_to_cell_enc import *
 
-def count_byte_frequency(hashed_pwds: list[str], freq_map: dict[str, int]) -> None:
+def count_byte_frequency(hashed_pwds: list[str], freq_map: dict[int, int]) -> None:
+    """
+    Counts the number of times that the same byte has occured in <hashed_pwds> 
+    and modifies <freq_map>.
+
+    Parameters
+    ----------
+    hashed_pwds : list[str]
+        An array pf hashed passwords.
+    freq_map : dict[int, int]
+        A dictionary where the keys are the ordinal values of ASCII characters
+        and the value is the freqeuncy of that ASCII character to occur.
+    
+    Return
+    -------
+    None
+    """
     for pwd_str in hashed_pwds:
         for char in pwd_str:
             freq_map[ord(char)] += 1
     
 
-def hist_of_byte_freq(freq_map: dict[str, int], rule_set_name: str, gen_num: str) -> None:
+def hist_of_byte_freq(freq_map: dict[int, int], rule_set_name: str, gen_num: str) -> None:
+    """
+    Plots a histogram to show the data from <freq_map> given the rule set name, 
+    <rule_set_name>, and the generation number, <gen_num>. 
+
+    Parameters
+    ----------
+    freq_map : dict[int, int]
+        A dictionary where the keys are the ordinal values of ASCII characters
+        and the value is the freqeuncy of that ASCII character to occur.
+    rule_set_name : str
+        The name of the rule set used during the experiment.
+    gen_num : str
+        The number of generations in string format.
     
+    Return
+    -------
+    None
+    """
     ascii_chars = list(freq_map.keys())
     char_freq = list(freq_map.values())
 
     plt.bar(ascii_chars, char_freq)
-    plt.title(f"Frequency of ASCII Characters for 10,000 CA Hashed Passwords using Rule {rule_set_name} for {gen_num} generations")
+    plt.title(f"Frequency of ASCII Characters for 10,000 CA Hashed Passwords", 
+              "using Rule {rule_set_name} for {gen_num} generations")
     plt.xlabel("ASCII Characters in Decimal Form")
     plt.ylabel("Frequency")
     plt.show()
 
-def chi_square_statistc(observed_freq: dict[str, int], expected_freq: int) -> None:
+def chi_square_statistc(observed_freq: dict[int, int], expected_freq: int) -> None:
+    """
+    Determines and plots the chi-square using <observed_freq> and the 
+    <expected_freq>. 
+
+    Parameters
+    ----------
+    observed_freq : dict[int, int]
+        A dictionary where the keys are the ordinal values of ASCII characters
+        and the value is the freqeuncy of that ASCII character to occur.
+    expected_freq : int
+        The expected count of ASCII characters for an ideal hash function to 
+        produce. 
+    
+    Return
+    -------
+    None
+    """
     # Calculate Chi-Square statistic for our observed freqency of byte compared
     # to the expected frequency of byte.
 
@@ -67,6 +126,22 @@ def chi_square_statistc(observed_freq: dict[str, int], expected_freq: int) -> No
     plt.show()
 
 def byte_pair_heatmap(hashed_pwds: list[str], num_gen: str) -> None:
+    """
+    Counts the frequency of the number of byte pairs that occur within 
+    <hashed_pwds> and plots a heatmap given <num_gen>. 
+
+    Parameters
+    ----------
+    hashed_pwds : list[str]
+        An array pf hashed passwords.
+    num_gen: str
+        The number of generations that an input password went through in string 
+        format.
+    
+    Return
+    -------
+    None
+    """
     byte_map = [[0 for _ in range(256)] for _ in range(256)]
 
     for pwd in hashed_pwds:
@@ -74,13 +149,29 @@ def byte_pair_heatmap(hashed_pwds: list[str], num_gen: str) -> None:
             byte_map[ord(pwd[char_idx])][ord(pwd[char_idx + 1])] += 1
     
     plt.imshow(byte_map, cmap='viridis')
-    plt.title(f"Byte Pair Heatmap Of 10,000 Hashed Passwords Using Rule 30 For {num_gen} Generations")
+    plt.title(f"Byte Pair Heatmap Of 10,000 Hashed Passwords Using Rule 30 For", 
+              "{num_gen} Generations")
     plt.xlabel("Ordinal Values of ASCII Characters (0-255)")
     plt.ylabel("Ordinal Values of ASCII Characters (0-255)")
     plt.colorbar()
     plt.show()
 
 def bit_balance_statistic(hashed_pwds: list[str]) -> tuple[float, float]:
+    """
+    Returns a tuple of the percentage that a 0 bit occurs within a hashed 
+    password and the percentage that a 1 bit occurs.
+
+    Parameter
+    ----------
+    hashed_pwds : list[str]
+        An array of hashed passwords.
+    
+    Return
+    ------
+    tuple[float, float]
+        Represents the two percentages of the number of times that a 0 bit 
+        occurs and a 1 bit occurs respectively.
+    """
     binary_hashed_pwd = [txt_to_bin(pwd) for pwd in hashed_pwds]
     bit_freq = {"1": 0, "0": 0}
 
@@ -95,4 +186,25 @@ def bit_balance_statistic(hashed_pwds: list[str]) -> tuple[float, float]:
     bit_zero_percentage = 1.0 - bit_one_percentage
 
     return (bit_zero_percentage, bit_one_percentage)
+
+def hash_collision_statistic(hashed_pwds: list[str]) -> int:
+    """
+    Returns an integer that represents the number of times that there has been
+    a hash collision in <hashed_pwds>. 
+
+    Parameter
+    ---------
+    hashed_pwds : list[str]
+        An array of hashed passwords.
     
+    Return
+    ------
+    int 
+        Represents the number of times that a hash collision has occured.
+    """
+    num_duplicate = 0
+    for pwd_idx in range(len(hashed_pwds) - 1):
+        if hashed_pwds[pwd_idx] == hashed_pwds[pwd_idx + 1]:
+            num_duplicate += 1
+    
+    return num_duplicate
